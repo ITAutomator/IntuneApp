cddb8286eeeac5e6b815c5b34947996fa9df1b45
ITAutomator <github@itautomator.com>
2025-09-30 19:30:09 -0400

    Synced via GitHubSync

--- Windows VPN/IntuneApp/IntuneUtils/intune_detection.ps1
+++ Windows VPN/IntuneApp/IntuneUtils/intune_detection.ps1
@@ -32,7 +32,7 @@ Function IntuneAppValues
 {
     # These values are replaced by AppsPublish.ps1 with matching values from the CSV file
 	$IntuneAppValues = @{}
-    $IntuneAppValues.Add("AppName","Windows VPN-v131")
+    $IntuneAppValues.Add("AppName","Windows VPN-v132")
     $IntuneAppValues.Add("AppInstaller","ps1")
     $IntuneAppValues.Add("AppInstallName","WindowsVPN.ps1")
     $IntuneAppValues.Add("AppInstallArgs","ARGS:-mode A")
